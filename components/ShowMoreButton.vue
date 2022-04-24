a144cde225e73f157415ab2593947cb71f905184
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-04-24 15:53:33 +0900

    build: pc layout create

---
+++ components/ShowMoreButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="justify-end">
+    <v-btn block text color="#F44336" class="v-btn_content">
+      もっと見る ∨
+    </v-btn>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ShowMoreButton',
+  css: ['~/assets/variables.scss'],
+}
+</script>
---
+++ pages/offices.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container class="pt-10 item-list">
+    <div>
+      <v-card width="520" height="423"> 画像 </v-card>
+      <v-card width="520" height="491" class="my-6"> 事業所タイトル </v-card>
+      <v-card width="520" height="599" class="my-6">
+        スタッフ紹介
+        <show-more-button />
+      </v-card>
+      <v-card width="520" height="513" class="my-6"> 施設概要 </v-card>
+    </div>
+    <div>
+      <v-card width="438" height="580" class="ml-8 sticky"> 事業所情報 </v-card>
+    </div>
+  </v-container>
+</template>
+<script>
+export default {
+  css: ['~/assets/variables.scss'],
+
+  layout: 'top',
+}
+</script>
