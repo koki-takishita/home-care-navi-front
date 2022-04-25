c94627429ab65cba3954957464c4cc58a9c6bfb0
koki-takishita <tkkokitk@yahoo.co.jp>
2022-04-26 01:09:36 +0900

    fix: change pages/index.vue to pages/top.vue for root path

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,16 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: 'top',
+        path: '/',
+        component: resolve(__dirname, 'pages/top.vue'),
+      })
+    },
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - front',
--- pages/top.vue
+++ pages/top.vue
@@ -6,6 +6,6 @@
 </template>
 <script>
 export default {
-  layout: 'top',
+  layout: 'application',
 }
 </script>
