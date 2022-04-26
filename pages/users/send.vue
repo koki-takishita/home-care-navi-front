a1ae88b2e900a52673edc3cc6bf2f3f4d019e739
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-04-26 14:40:33 +0900

    build: toppage transition

--- pages/users/send.vue
+++ pages/users/send.vue
@@ -1,12 +1,26 @@
 <template>
-  <v-card width="750" class="mx-auto mt-2">
-    <v-card-title>
-      <h1 class="display-1">仮登録完了</h1>
-    </v-card-title>
+  <v-card outlined width="750" class="mx-auto mt-10">
+    <v-card-text>
+      <h1 class="text-center mt-8 title-text">仮登録完了</h1>
+      <div class="text-center mt-12">
+        <p>
+          入力いただいたメールアドレスに<br />
+          確認メールを送付いたしました。
+        </p>
+      </div>
+      <a
+        href="../top"
+        class="mx-auto mt-4 text-center top-link mb-16"
+        style="color: #f06364"
+      >
+        <p>ホームケアナビトップに戻る</p>
+      </a>
+    </v-card-text>
   </v-card>
 </template>
 <script>
 export default {
   layout: 'application',
+  css: ['~/assets/variables.scss'],
 }
 </script>
