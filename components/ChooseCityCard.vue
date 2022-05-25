3af552e73b576efd59f90abb7dc43d3a12b22c4d
koki-takishita <tkkokitk@yahoo.co.jp>
2022-05-25 16:02:23 +0900

    fix: style 調整

--- components/ChooseCityCard.vue
+++ components/ChooseCityCard.vue
@@ -2,26 +2,37 @@
   <v-card
     min-height="324"
     max-width="268"
+    outlined
     class="pa-6 pt-5 pb-3 d-flex flex-column"
   >
     <v-list class="overflow-auto mb-auto" max-height="240">
-      <v-list-item-group multiple>
+      <v-list-item-group>
         <v-list-item v-for="(city, i) in cities" :key="i" dense>
           <v-checkbox
             v-model="chooseItems"
+            class="mt-n1"
+            multiple
             dense
             :value="cities[i].city"
-          ></v-checkbox>
-          {{ city.city }}
+            hide-details
+          >
+          </v-checkbox>
+          <v-list-item-content class="text-button pa-0">
+            {{ city.city }}
+          </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
     </v-list>
     <div class="d-flex ml-n3">
-      <v-btn min-width="80" min-height="40" class="mr-2">クリア</v-btn>
+      <v-btn min-width="80" min-height="40" class="mr-2" outlined depressed>
+        <span class="color-red">クリア</span></v-btn
+      >
       <v-btn
         min-width="160"
         min-height="40"
         color="error"
+        depressed
+        class="font-weight-black"
         @click="SearchForOfficesChosenByAddress"
         >検索する</v-btn
       >
@@ -72,3 +83,8 @@ export default {
   },
 }
 </script>
+<style>
+.color-red {
+  color: red;
+}
+</style>
