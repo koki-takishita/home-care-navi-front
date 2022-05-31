5ac0a4c517c325e5b99f8fdd981b1c8306bfb2f6
tmhr0 <falsity_0330@yahoo.co.jp>
2022-05-31 12:12:27 +0900

    fix: ファイル名修正

--- pages/contacts/confirm.vue
+++ pages/contacts/confirm.vue
@@ -34,13 +34,13 @@
         </div>
         <v-card-actions>
           <v-btn
-            to="/contacts/sucsess"
             block
             large
             class="error text-h6 block"
             max-width="520"
             min-width="343"
             height="60"
+            @click="SendSuccessPage"
           >
             送信する
           </v-btn>
@@ -64,21 +64,40 @@ export default {
   layout: 'application',
   data() {
     return {
-      name: '',
-      email: '',
-      types: '',
-      content: '',
+      items: ['ユーザー', 'ケアマネージャー', '事業所', 'その他'],
+      form: {
+        name: '',
+        email: '',
+        types: '',
+        content: '',
+        valid: false,
+      },
+      formValidates: {
+        required: (value) => !!value || '必須項目です',
+        typeCheckString: (value) => {
+          const format = /^[a-zA-Z0-9]+$/g
+          return format.test(value) || '入力できるのは半角英数字のみです'
+        },
+        email: (value) => {
+          const format =
+            // eslint-disable-next-line no-control-regex
+            /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|\\[\x01-\x09\x0B\x0C\x0E-\x7F])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21-\x5A\x53-\x7F]|\\[\x01-\x09\x0B\x0C\x0E-\x7F])+)\])$/g
+          return format.test(value) || '正しいメールアドレスを入力してください'
+        },
+      },
     }
   },
-  mounted() {
-    this.setParameter()
-  },
   methods: {
-    setParameter() {
-      this.name = this.$route.query.name
-      this.email = this.$route.query.email
-      this.types = this.$route.query.types
-      this.content = this.$route.query.content
+    SendSuccessPage() {
+      this.$router.push({
+        path: '/contacts/success',
+        query: {
+          name: this.form.name,
+          email: this.form.email,
+          types: this.form.types,
+          content: this.form.content,
+        },
+      })
     },
   },
 }
