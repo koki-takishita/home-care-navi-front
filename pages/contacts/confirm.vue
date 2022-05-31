c437144d6639a23bf90633222960b02123925f39
tmhr0 <falsity_0330@yahoo.co.jp>
2022-05-31 12:52:07 +0900

    fix: パス修正

--- pages/contacts/confirm.vue
+++ pages/contacts/confirm.vue
@@ -64,40 +64,32 @@ export default {
   layout: 'application',
   data() {
     return {
-      items: ['ユーザー', 'ケアマネージャー', '事業所', 'その他'],
-      form: {
-        name: '',
-        email: '',
-        types: '',
-        content: '',
-        valid: false,
-      },
-      formValidates: {
-        required: (value) => !!value || '必須項目です',
-        typeCheckString: (value) => {
-          const format = /^[a-zA-Z0-9]+$/g
-          return format.test(value) || '入力できるのは半角英数字のみです'
-        },
-        email: (value) => {
-          const format =
-            // eslint-disable-next-line no-control-regex
-            /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|\\[\x01-\x09\x0B\x0C\x0E-\x7F])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21-\x5A\x53-\x7F]|\\[\x01-\x09\x0B\x0C\x0E-\x7F])+)\])$/g
-          return format.test(value) || '正しいメールアドレスを入力してください'
-        },
-      },
+      name: '',
+      email: '',
+      types: '',
+      content: '',
     }
   },
+  mounted() {
+    this.setParameter()
+  },
   methods: {
-    SendSuccessPage() {
-      this.$router.push({
-        path: '/contacts/success',
-        query: {
-          name: this.form.name,
-          email: this.form.email,
-          types: this.form.types,
-          content: this.form.content,
-        },
-      })
+    setParameter() {
+      this.name = this.$route.query.name
+      this.email = this.$route.query.email
+      this.types = this.$route.query.types
+      this.content = this.$route.query.content
+    },
+    async SendSuccessPage() {
+      try {
+        const response = await this.$axios.$post(`contacts`, {
+          test: 'test',
+        })
+        this.$router.push('/contacts/success')
+        return response
+      } catch (error) {
+        return error
+      }
     },
   },
 }
