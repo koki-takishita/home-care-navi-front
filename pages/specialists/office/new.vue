85c7ca7b6271c5279160617a0801415d867c5748
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-06-01 16:48:17 +0900

    build: validation checkbox flags

--- pages/specialists/office/new.vue
+++ pages/specialists/office/new.vue
@@ -2,11 +2,13 @@
   <div>
     <v-card class="mx-auto mb-2 p-0" width="750">
       <v-col cols="12"><h3>事業所登録</h3></v-col>
-      <v-form>
+      <v-form v-model="valid">
         <v-col cols="12">
           <label class="font-color-gray font-weight-black text-caption"
             >事業所名
             <v-text-field
+              v-model="name"
+              :rules="[formValidates.required, formValidates.nameCountCheck]"
               class="mt-2 font-weight-regular"
               placeholder="ケアパークむかい風"
               outlined
@@ -16,6 +18,8 @@
           <label class="font-color-gray font-weight-black text-caption"
             >特徴タイトル
             <v-text-field
+              v-model="title"
+              :rules="[formValidates.required, formValidates.titleCountCheck]"
               class="mt-2 font-weight-regular"
               placeholder="事業所紹介タイトル"
               outlined
@@ -51,6 +55,11 @@
           <label class="font-color-gray font-weight-black text-caption"
             >営業日に関する説明
             <v-textarea
+              v-model="business_day_detail"
+              :rules="[
+                formValidates.required,
+                formValidates.businessDayDetailCountCheck,
+              ]"
               class="mt-2 font-weight-regular"
               placeholder="例｝ 第ニと第三土曜日は営業しています"
               height="105"
@@ -62,6 +71,8 @@
           <label class="font-color-gray font-weight-black text-caption"
             >電話番号
             <v-text-field
+              v-model="phone_number"
+              :rules="[formValidates.required, formValidates.phoneNumber]"
               class="mt-2 font-weight-regular"
               placeholder="080-1234-5678"
               outlined
@@ -71,6 +82,8 @@
           <label class="font-color-gray font-weight-black text-caption"
             >FAX
             <v-text-field
+              v-model="fax_number"
+              :rules="[formValidates.required, formValidates.phoneNumber]"
               class="mt-2 font-weight-regular"
               placeholder="090-8765-4321"
               outlined
@@ -79,6 +92,8 @@
           /></label>
         </v-col>
         <v-text-field
+          v-model="post_code"
+          :rules="[formValidates.required, formValidates.postCode]"
           outlined
           dense
           height="44"
@@ -94,6 +109,7 @@
         <v-col cols="12" class="pt-0">
           <div class="mt-n2">
             <v-text-field
+              v-model="address"
               outlined
               dense
               height="44"
@@ -102,6 +118,16 @@
             >
             </v-text-field>
           </div>
+          <v-btn
+            x-large
+            block
+            depressed
+            :disabled="!valid"
+            color="warning"
+            @click="send"
+          >
+            登録する
+          </v-btn>
         </v-col>
       </v-form>
     </v-card>
@@ -113,16 +139,63 @@ export default {
   layout: 'application_specialists',
   data() {
     return {
+      formValidates: {
+        required: (value) => !!value || '必須項目です',
+        nameCountCheck: (value) =>
+          value.length <= 30 || '30文字以下で入力してください',
+        titleCountCheck: (value) =>
+          value.length <= 50 || '50文字以下で入力してください',
+        businessDayDetailCountCheck: (value) =>
+          value.length <= 120 || '120文字以下で入力してください',
+        phoneNumber: (value) => {
+          const format = /^\d{2,4}-\d{2,4}-\d{4}$/g
+          return format.test(value) || '正しい電話番号ではありません'
+        },
+        postCode: (value) => {
+          const format = /^[0-9]{3}-[0-9]{4}$/g
+          return (
+            format.test(value) || '正しい郵便番号ではありません 例) 000-0000'
+          )
+        },
+      },
       name: '',
       title: '',
-      flags: '',
+      flags: 0,
       business_day_detail: '',
       phone_number: '',
       fax_number: '',
       post_code: '',
       address: '',
       selected: [],
+      valid: false,
     }
+  },
+  methods: {
+    send() {
+      if (this.selected.includes('日')) {
+        this.flags += 1
+      }
+      if (this.selected.includes('月')) {
+        this.flags += 2
+      }
+      if (this.selected.includes('火')) {
+        this.flags += 4
+      }
+      if (this.selected.includes('水')) {
+        this.flags += 8
+      }
+      if (this.selected.includes('木')) {
+        this.flags += 16
+      }
+      if (this.selected.includes('金')) {
+        this.flags += 32
+      }
+      if (this.selected.includes('土')) {
+        this.flags += 64
+      }
+      console.log(this.flags)
+      this.flags = 0
+    },
   },
 }
 </script>
