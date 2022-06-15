e13198f25db8e2a37c8f48fc013d72953da7f026
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-06-15 10:47:37 +0900

    fix: 秒数戻す

--- pages/offices/_id/appointments/index.vue
+++ pages/offices/_id/appointments/index.vue
@@ -274,7 +274,7 @@ export default {
     sendConfirmPage() {
       // 現在のUNIX時間から、有効期限を設定する(UNIX時間は単位が秒なので、秒数を足す)
       // https://keisan.casio.jp/exec/system/1526004418
-      const expiry = Math.floor(this.currentTime / 1000) + 60
+      const expiry = Math.floor(this.currentTime / 1000) + 180
       localStorage.setItem('appointments.meet_date', this.meet_date)
       localStorage.setItem('appointments.meet_time', this.meet_time)
       localStorage.setItem('appointments.name', this.name)
