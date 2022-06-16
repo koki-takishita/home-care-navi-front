5a1dcfa89a5a6834fe256b1750713bd017148c01
tomohiro-kushida <70782980+tmhr0@users.noreply.github.com>
2022-06-16 11:55:07 +0900

    Update new.vue
    > 441 バリテーション10MB以下で反応になっていたので、10MB以上で反応するよう一文字修正

--- pages/specialists/office/new.vue
+++ pages/specialists/office/new.vue
@@ -438,7 +438,7 @@ export default {
           !values.some((value) => value.size >= 10000000) ||
           '画像サイズは10MB以下でアップロードしてください',
         fileDetailSizeCheck: (value) =>
-          value.size >= 10000000 ||
+          value.size <= 10000000 ||
           '画像サイズは10MB以下でアップロードしてください',
         fileLengthCheck: (value) =>
           value.length <= 5 || '画像は5枚以下にしてください',
