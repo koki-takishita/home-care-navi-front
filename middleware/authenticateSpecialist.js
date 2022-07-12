609e58cedee2ceec852cf34fe6c27dbe7b838885
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-07-12 16:05:39 +0900

    fix: specialist_login_limit

--- middleware/authenticateSpecialist.js
+++ middleware/authenticateSpecialist.js
@@ -1,6 +1,7 @@
 export default function ({ store, redirect, route }) {
   if (
     route.path !== '/specialists/login' &&
+    route.path !== '/specialists/users/new' &&
     store.state.specialist !== 'true'
   ) {
     console.log('ミドルウェア')
