705711cdc3b273bb66dcf69b062eee94254fef35
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-07-13 12:28:49 +0900

    fix: add_send_page

--- middleware/authenticateSpecialist.js
+++ middleware/authenticateSpecialist.js
@@ -2,6 +2,7 @@ export default function ({ store, redirect, route }) {
   if (
     route.path !== '/specialists/login' &&
     route.path !== '/specialists/users/new' &&
+    route.path !== '/specialists/users/send' &&
     store.state.specialist !== 'true'
   ) {
     console.log('ミドルウェア')
