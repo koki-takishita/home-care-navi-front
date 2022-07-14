20ad4f90ee1f82852be72914a1e5be37645e79db
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-07-14 16:53:10 +0900

    build: テストのスラッシュ削除

--- middleware/authenticateSpecialist.js
+++ middleware/authenticateSpecialist.js
@@ -1,7 +1,6 @@
 export default function ({ store, redirect, route }) {
   if (
     route.path !== '/specialists/login' &&
-    route.path !== '/specialists/login/' &&
     route.path !== '/specialists/users/new' &&
     route.path !== '/specialists/users/send' &&
     store.state.specialist !== 'true'
