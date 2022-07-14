378b69a0f67bd8a79cf0110e9811e2a762a31a0a
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-07-14 12:35:56 +0900

    build: テスト通るよう修正

--- middleware/authenticateSpecialist.js
+++ middleware/authenticateSpecialist.js
@@ -1,6 +1,7 @@
 export default function ({ store, redirect, route }) {
   if (
     route.path !== '/specialists/login' &&
+    route.path !== '/specialists/login/' &&
     route.path !== '/specialists/users/new' &&
     route.path !== '/specialists/users/send' &&
     store.state.specialist !== 'true'
