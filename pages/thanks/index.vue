c10c6a672010fce276d24719c42f2f54db0f6b51
koki-takishita <tkkokitk@yahoo.co.jp>
2022-07-21 11:48:58 +0900

    build: お礼履歴修正

--- pages/thanks/index.vue
+++ pages/thanks/index.vue
@@ -1,8 +1,135 @@
 <template>
-  <h1>お礼投稿ページ</h1>
+  <v-container class="base-width">
+    <v-card outlined class="cards-wrapper">
+      <v-card-title class="text-h6 font-weight-black pl-0"
+        >お礼投稿履歴</v-card-title
+      >
+      <v-row v-if="thanksExist">
+        <v-col v-for="thank in thanks" :key="thank.id" cols="12" md="6"
+          ><ThankCard :thank="thank" />
+        </v-col>
+        <v-col cols="12">
+          <v-pagination
+            v-model="page"
+            :length="count"
+            color="#D9DEDE"
+            class="page-nation"
+          ></v-pagination>
+        </v-col>
+      </v-row>
+      <div v-else class="not-thanks-comment mt-16">
+        <p class="text-center text-subtitle-1">お礼投稿履歴はありません</p>
+        <ThankBackLink
+          class="text-center"
+          :text="backText"
+          @movePage="moveTop"
+        />
+      </div>
+    </v-card>
+  </v-container>
 </template>
 <script>
 export default {
   layout: 'application',
+  asyncData({ query }) {
+    const page = Number(query.page) || 1
+    return page
+  },
+  data() {
+    return {
+      thanks: [],
+      page: 1,
+      count: '',
+      loading: true,
+    }
+  },
+  computed: {
+    thanksExist() {
+      return this.thanks.length > 0
+    },
+    backText() {
+      return 'ホームケアナビトップに戻る'
+    },
+  },
+  watch: {
+    page() {
+      this.fetchThanks(this.page)
+      this.scrollTop()
+      this.$router.push({
+        path: '/thanks',
+        query: {
+          page: this.page,
+        },
+      })
+    },
+  },
+  mounted() {
+    this.fetchThanks(this.page)
+  },
+  methods: {
+    async fetchThanks(page = 1) {
+      try {
+        const offsetPage = page - 1
+        const res = await this.$axios.$get(`thanks?page=${offsetPage}`)
+        this.thanks = res.thank
+        let count = res.count
+        count = count / 10 || 0
+        count = Math.ceil(count)
+        if (count === 0) {
+          count = 1
+        }
+        this.count = count
+      } catch (error) {
+        // console.log(error)
+        return error
+      }
+    },
+    moveTop() {
+      this.$router.push('/')
+    },
+    scrollTop() {
+      this.$vuetify.goTo(0)
+    },
+  },
 }
 </script>
+<style scoped>
+.base-width {
+  max-width: 990px;
+}
+
+.cards-wrapper {
+  border: 0;
+  background-color: #f5f7f7;
+}
+
+.not-thanks-comment {
+  color: #6d7570;
+}
+
+/* stylelint-disable */
+.loading-element {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 2;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
+
+::v-deep button.v-pagination__navigation {
+  box-shadow: none;
+  border: 1px solid #d9dede;
+}
+
+::v-deep button.v-pagination__item {
+  box-shadow: none;
+  border: 1px solid #d9dede;
+}
+
+::v-deep i.v-icon.notranslate.mdi {
+  color: #f06364;
+}
+/* stylelint-enable */
+</style>
