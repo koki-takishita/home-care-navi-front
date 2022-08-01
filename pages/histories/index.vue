d865f6f9467e5f9734fb797560c4bd87093e4473
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-08-01 21:07:57 +0900

    fix: 閲覧履歴ありません表示

--- pages/histories/index.vue
+++ pages/histories/index.vue
@@ -1,12 +1,18 @@
 <template>
   <v-col cols="12" md="8" lg="7" xl="7" class="mx-auto">
     <h3>閲覧履歴</h3>
-    <p class="font-weight-black">{{ conversionStringToOfficeCount }}</p>
-    <v-row class="mt-4">
-      <v-col v-for="(office, index) in offices" :key="index" cols="12" md="6">
-        <officeCard :office="office" />
-      </v-col>
-    </v-row>
+    <div v-if="historiesExist">
+      <p class="font-weight-black">{{ conversionStringToOfficeCount }}</p>
+      <v-row class="mt-4">
+        <v-col v-for="(office, index) in offices" :key="index" cols="12" md="6">
+          <officeCard :office="office" />
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else class="not-histories-comment mt-16">
+      <p class="text-center text-subtitle-1">閲覧履歴はありません</p>
+      <ThankBackLink class="text-center" :text="backText" @movePage="moveTop" />
+    </div>
   </v-col>
 </template>
 
@@ -17,12 +23,8 @@ export default {
   async asyncData({ $axios }) {
     try {
       const res = await $axios.$get(`histories`)
-      let officeCount = 0
-      if (res.length !== 0) {
-        officeCount = res.length
-      }
       return {
-        officeCount,
+        officeCount: res.length,
         offices: res,
       }
     } catch (error) {
@@ -33,10 +35,19 @@ export default {
     conversionStringToOfficeCount() {
       return '最新' + this.officeCount + '件'
     },
+    historiesExist() {
+      return this.officeCount > 0
+    },
+    backText() {
+      return 'ホームケアナビトップに戻る'
+    },
   },
   methods: {
     moveShow(id) {
       this.$router.push({ path: `/offices/${id}` })
+    },
+    moveTop() {
+      this.$router.push('/')
     },
   },
 }
@@ -49,5 +60,9 @@ export default {
 .set-max-layout {
   max-height: 50px;
   line-height: normal;
+}
+
+.not-histories-comment {
+  color: #6d7570;
 }
 </style>
