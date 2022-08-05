6ee44d57d20c14351f99c235ef2830b4487dca00
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-05 14:51:02 +0900

    build: ch webpack version 5 to 4.46.0

--- pages/users/new.vue
+++ pages/users/new.vue
@@ -228,7 +228,7 @@ export default {
           phone_number: this.form.phone_number,
           post_code: this.form.post_code,
           address: this.form.address,
-          confirm_success_url: this.$config.frontTopUrl,
+          confirm_success_url: `${this.$config.frontTopUrl}/top`,
         })
         this.$router.push('/users/send')
         return response
