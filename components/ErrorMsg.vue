09c629e7424360c1aa124dfd6050ab4bf743c8cb
koki-takishita <tkkokitk@yahoo.co.jp>
2022-08-15 12:39:15 +0900

    fix: 成功: 5秒後に消える、失敗: 残り続ける

--- components/ErrorMsg.vue
+++ components/ErrorMsg.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="my-2">
     <v-alert
-      v-for="(msg, i) in getMsg"
+      v-for="(msg, i) in GetMsg"
       :key="i"
-      :type="getType"
+      :type="GetType"
       max-width="750"
       min-width="350"
       dismissible
       class="mx-auto mb-2"
+      :value="alert"
     >
       {{ msg }}
     </v-alert>
@@ -18,19 +19,31 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      msgs: [],
-      type: 'error',
+      // v-alertコンポネント表示するかしないかのフラグ
+      // vale props 参照
+      // https://vuetifyjs.com/ja/api/v-alert/
+      alert: false,
     }
   },
   computed: {
-    ...mapGetters('catchErrorMsg', ['getMsg', 'getType', 'emptyState']),
-  },
-  watch: {
-    getMsg() {
+    ...mapGetters('catchErrorMsg', ['getMsg', 'getType']),
+    GetMsg() {
       return this.getMsg
     },
-    getType() {
+    GetType() {
       return this.getType
+    },
+  },
+  watch: {
+    // リクエストが成功: 指定した時間後にフラッシュ消える
+    // リクエストが失敗: 残り続ける ※エラー内容を読んでもらうたため
+    GetMsg() {
+      this.alert = true
+      if (this.GetType === 'success') {
+        setTimeout(() => {
+          this.alert = false
+        }, 5000)
+      }
     },
   },
 }
