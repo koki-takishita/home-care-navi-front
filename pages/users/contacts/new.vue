7cad2c2e17af54744a39513932f5be042ed52a45
suwonkun <suwon.nunya-14@softbank.ne.jp>
2022-08-31 12:19:22 +0900

    fix: 本番push

--- pages/users/contacts/new.vue
+++ pages/users/contacts/new.vue
@@ -57,20 +57,6 @@ export default {
       email: '',
       types: '',
       content: '',
-      valid: false,
-      formValidates: {
-        required: (value) => !!value || '必須項目です',
-        nameCountCheck: (value) =>
-          value.length <= 30 || '30文字以下で入力してください',
-        emailCountCheck: (value) =>
-          value.length <= 255 || '255文字以下で入力してください',
-        email: (value) => {
-          const format =
-            // eslint-disable-next-line no-control-regex
-            /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|\\[\x01-\x09\x0B\x0C\x0E-\x7F])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21-\x5A\x53-\x7F]|\\[\x01-\x09\x0B\x0C\x0E-\x7F])+)\])$/g
-          return format.test(value) || '正しいメールアドレスを入力してください'
-        },
-      },
     }
   },
   methods: {
